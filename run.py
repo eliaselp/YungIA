97d4a8df45309af4e090007292df5db6b6c4fff9
Elias Eduardo Liranza Pérez <146482192+eliaselp@users.noreply.github.com>
2024-08-26 21:19:30 -0700

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
 import os
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '1'
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+import tensorflow as tf
+config = tf.compat.v1.ConfigProto(device_count={'GPU': 0})
+session = tf.compat.v1.Session(config=config)
+tf.compat.v1.keras.backend.set_session(session)
+
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
 import yung_Coinex_LocalMaxMin as yung
 
